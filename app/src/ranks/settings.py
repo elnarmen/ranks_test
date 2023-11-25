d0e17eca00ac5a057e5891d37bec33921ecf949d
Elnar Mengelbaev <elnar_men@mail.ru>
2023-11-25 15:27:09 +0300

    Create payment_api app

--- app/src/ranks/settings.py
+++ app/src/ranks/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "payment_api.apps.PaymentApiConfig",
 ]
 
 MIDDLEWARE = [
