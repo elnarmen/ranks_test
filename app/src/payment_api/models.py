186eeb79790f91d721c650bf1d3af40f4a4a4d41
Эльнар <elnar_men@mail.ru>
2023-11-25 15:36:06 +0300

    Add Item model

---
+++ app/src/payment_api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField("Название", max_length=150)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Стоимость", max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
